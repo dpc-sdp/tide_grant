c5533a0863aa8a2626fcd6cbb9a17916b99c2864
Md Nadim Hossain <getovic@gmail.com>
2023-07-06 16:08:04 +1000

    [SRM-958] Updated grant to be compatible with d10.

--- dev-tools.sh
+++ dev-tools.sh
@@ -34,5 +34,6 @@
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
 # export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=e57446d1406c2c345d7e87a70a23ea58163ad619
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
